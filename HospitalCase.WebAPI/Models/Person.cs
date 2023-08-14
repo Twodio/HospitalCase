76d274314bdb9173bb05d926b7f84b3bf7113001
Twodio <edilsonsamuel12@hotmail.com>
2023-08-14 11:38:21 +0100

    Added the Photo (base64) property to the Person entity

--- HospitalCase.WebAPI/Models/Person.cs
+++ HospitalCase.WebAPI/Models/Person.cs
@@ -14,5 +14,7 @@ namespace HospitalCase.WebAPI.Models
         public string PhoneNumber { get; set; }
 
         public string CPF { get; set; }
+
+        public string PhotoBase64 { get; set; }
     }
 }
